8f66bbdaed24009a1bd3d003a79a96939ee89c4b
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-09-29 14:53:18 +0100

    Solved challenge 3, 4, 5

--- src/routes/confessions.js
+++ src/routes/confessions.js
@@ -3,17 +3,23 @@ const {
   createConfession,
 } = require("../model/confessions.js");
 const { Layout } = require("../templates.js");
+const { getSession } = require("../model/session.js");
 
 function get(req, res) {
-  /**
-   * Currently any user can view any other user's private confessions!
-   * We need to ensure only the logged in user can see their page.
-   * [1] Get the session ID from the cookie
-   * [2] Get the session from the DB
-   * [3] Get the logged in user's ID from the session
-   * [4] Get the page owner from the URL params
-   * [5] If the logged in user is not the page owner send a 401 response
-   */
+  // Get the session ID from the cookie
+  const sid = req.signedCookies.sid;
+  // Get the session from the DB
+  const session = getSession(sid);
+  // Get the logged in user's ID from the session
+  const currentUser = session && session.user_id;
+
+  //  Get the page owner from the URL params
+  const pageOwner = Number(req.params.user_id);
+
+  // If the logged in user is not the page owner send a 401 response
+  if(currentUser !== pageOwner) {
+    return res.status(401).send("<h1>You aren't allowed to see that</h1>")
+  }
   const confessions = listConfessions(req.params.user_id);
   const title = "Your secrets";
   const content = /*html*/ `
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,18 +1,18 @@
+const { getSession } = require("../model/session.js");
 const { Layout } = require("../templates.js");
 
 function get(req, res) {
-  /**
-   * [1] Read session ID from cookie
-   * [2] Get session from DB
-   * [3] If the session exists render a log out form
-   * [4] This should submit a request to `POST /log-out`
-   * [5] Else render the sign up/log in links
-   */
+  const sid = req.signedCookies.sid;
+  const session = getSession(sid);
   const title = "Confess your secrets!";
   const content = /*html*/ `
     <div class="Cover">
       <h1>${title}</h1>
-      <nav><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>
+      ${
+        session
+          ? /*html*/ `<form method="POST" action="/log-out"><button class="Button">Log out</button></form>`
+          : /*html*/ `<nav><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>`
+      }
     </div>
   `;
   const body = Layout({ title, content });
