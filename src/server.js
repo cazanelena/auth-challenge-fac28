a2e8a72c63f463c930fa01a728da64f41db20f2e
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-09-29 15:50:32 +0100

    solved challenge 7

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const signup = require("./routes/sign-up.js");
 const login = require("./routes/log-in.js");
 const logout = require("./routes/log-out.js");
 const confessions = require("./routes/confessions.js");
+const { getSession, removeSession } = require("./model/session.js");
 
 const body = express.urlencoded({ extended: false });
 const cookies = cookieParser(process.env.COOKIE_SECRET);
@@ -17,6 +18,24 @@ server.use((req, res, next) => {
   next();
 });
 server.use(cookies);
+server.use(sessions);
+
+function sessions(req, res, next) {
+  const sid = req.signedCookies.sid;
+  const session = getSession(sid);
+  if (session) {
+    const expiry = new Date(session.expires_at);
+    const today = new Date();
+    if (expiry < today) {
+      removeSession(sid);
+      res.clearCookie("sid");
+    } else {
+      req.session = session;
+    }
+  }
+  next();
+}
+
 server.get("/", home.get);
 server.get("/sign-up", signup.get);
 server.post("/sign-up", body, signup.post);
